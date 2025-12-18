0fc9470d1ebbfdba68b565ecf63f7eddc6fac862
Clément Robert <cr52@protonmail.com>
2025-12-18 08:02:26 +0100

    API: deprecate legacy default value in `niter_lic`, deprecate `kernel_length` and `light_source`, add support for arbitrary LIC kernels and post-processors

--- tests/test_interface.py
+++ tests/test_interface.py
@@ -45,8 +45,9 @@ def test_indexing(indexing):
         xmax=9,
         ymin=1,
         ymax=4,
+        kernel=np.sin(np.linspace(0, np.pi, 3)),
         niter_lic=1,
-        kernel_length=3,
+        post_lic="north-west-light-source",
     )
 
 
